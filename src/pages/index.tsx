828bbc78586e8237f74aa8c91c80d04bae0ab347
richseviora <richard.seviora@gmail.com>
2020-09-14 15:19:35 -0700

    [RDE-321] Core initial layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
 import React from "react"
+import { Layout } from "../components/layout"
+import styles from "./index.module.scss"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <h1>Olive Helps</h1>
+    </Layout>
+  )
 }
