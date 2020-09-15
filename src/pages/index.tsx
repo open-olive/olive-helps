5a3398966d712be4e10e31c8d538f4a5d3b16822
richseviora <richard.seviora@gmail.com>
2020-09-15 11:57:35 -0700

    [RDE-321] Add more content, styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,17 +4,82 @@ import styles from "./index.module.scss"
 import "./index.scss"
 import { PageHeader } from "../components/page-header"
 import logo from "./olive-helps-logo-white.png"
+import {
+  downloadLinuxUrl,
+  downloadMacUrl,
+  downloadWindowsUrl,
+  goGitHubUrl,
+  nodeGitHubUrl,
+  nodeLDKUrl,
+} from "../references"
+import { Section } from "../components/section"
 
 export default function Home() {
   const title = (
     <>
-      <img src={logo} width={500}/> <br />
+      <img src={logo} width={500} /> <br />
       Developer Hub
     </>
   )
   return (
     <Layout>
       <PageHeader title={title} />
+      <Section>
+        <h2 className={styles.sectionTitle}>Download</h2>
+        <p>Download the Olive Helps app.</p>
+        <div className={styles.downloadCollection}>
+          <article className={styles.downloadItem}>
+            <a href={downloadWindowsUrl} className={styles.downloadLink}>
+              Windows
+            </a>
+          </article>
+          <article className={styles.downloadItem}>
+            <a className={styles.downloadLink} href={downloadMacUrl}>
+              MacOS
+            </a>
+          </article>
+          <article className={styles.downloadItem}>
+            <a className={styles.downloadLink} href={downloadLinuxUrl}>
+              Linux
+            </a>
+          </article>
+        </div>
+      </Section>
+      <Section sectionClassName={styles.alternatingSectionBackground}>
+        <h2 className={styles.sectionTitle}>Get the LDK</h2>
+        <p>Get the LDK and start building!</p>
+        <div className={styles.downloadCollection}>
+          <article className={styles.downloadItem}>
+            <h3 className={styles.languageTitle}>Node</h3>
+            <a href={nodeGitHubUrl} className={styles.ldkLink}>
+              GitHub
+            </a>
+            <br />
+            <a href={nodeLDKUrl} className={styles.ldkLink}>
+              Documentation
+            </a>
+          </article>
+          <article className={styles.downloadItem}>
+            <h3 className={styles.languageTitle}>Go</h3>
+            <a href={goGitHubUrl} className={styles.ldkLink}>
+              GitHub
+            </a>
+          </article>
+        </div>
+      </Section>
+      <Section>
+        <h2 className={styles.sectionTitle}>Getting Help</h2>
+        <p>
+          Email your Olive Helps developer contact if you need any help! We're
+          here to help you succeed!
+        </p>
+      </Section>
     </Layout>
   )
 }
+
+/**
+ * LDK Links
+ * LDK Documentation Links
+ * Download Links
+ */
---
+++ src/references.ts
@@ -0,0 +1,6 @@
+export const downloadWindowsUrl = "https://olive.page.link/olive-helps-windows"
+export const downloadLinuxUrl = "https://olive.page.link/olive-helps-linux"
+export const downloadMacUrl = "https://olive.page.link/olive-helps-mac"
+export const nodeGitHubUrl = "https://github.com/open-olive/loop-development-kit-node";
+export const goGitHubUrl = "https://github.com/open-olive/loop-development-kit-go";
+export const nodeLDKUrl = "https://open-olive.github.io/loop-development-kit-node/";
