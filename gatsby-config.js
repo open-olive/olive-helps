b802bb729978cbf9391bf5cc3b449cac4095303c
richseviora <richard.seviora@gmail.com>
2020-09-15 11:04:00 -0700

    [RDE-321] Various changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,6 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: ['gatsby-plugin-sass'],
   pathPrefix: '/open-olive'
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
 import React from "react"
 import { Layout } from "../components/layout"
 import styles from "./index.module.scss"
+import "./index.scss"
+import { PageHeader } from "../components/page-header"
+import logo from "./olive-helps-logo-white.png"
 
 export default function Home() {
+  const title = (
+    <>
+      <img src={logo} width={500}/> <br />
+      Developer Hub
+    </>
+  )
   return (
     <Layout>
-      <h1>Olive Helps</h1>
+      <PageHeader title={title} />
     </Layout>
   )
 }
