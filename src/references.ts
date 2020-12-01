b2d13f252e866b658326d1c1b0298a01788061fc
richseviora <richard.seviora@gmail.com>
2020-12-01 10:45:52 -0800

    [SIDE-884] Add .Net repo, update links

--- src/references.ts
+++ src/references.ts
@@ -1,10 +1,11 @@
 export const downloadWindowsUrl = "https://olive.page.link/olive-helps-windows"
 export const downloadMacUrl = "https://olive.page.link/olive-helps-mac"
 export const nodeGitHubUrl =
-  "https://github.com/open-olive/loop-development-kit-node"
+  "https://github.com/open-olive/loop-development-kit/tree/main/ldk/node"
 export const goGitHubUrl =
-  "https://github.com/open-olive/loop-development-kit-go"
+  "https://github.com/open-olive/loop-development-kit/tree/main/ldk/go"
 export const nodeLDKUrl =
   "https://open-olive.github.io/loop-development-kit-node/"
 export const goLDKUrl =
-  "https://pkg.go.dev/github.com/open-olive/loop-development-kit-go"
+  "https://pkg.go.dev/github.com/open-olive/loop-development-kit/ldk/go"
+export const dotNetLDKUrl = "https://github.com/open-olive/loop-development-kit/tree/main/ldk/csharp/OliveHelpsLDK"
